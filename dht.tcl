1c1a580f0b7178866f51c48902df8f48510f6348
nuccleon <github@claus-beckenbauer.de>
2017-12-01 19:13:56 +0100

    exculde reading values

---
+++ dht.tcl
@@ -0,0 +1,11 @@
+#!/usr/bin/expect
+# set reading(DEVICE) { reading1 reading2 reading3 }
+set readings(dht.aussen) {temperature humidity humidity.abs}
+set readings(dht.lunos) {temperature humidity humidity.abs}
+set readings(dht.k1) {temperature humidity humidity.abs}
+set readings(dht.k3) {temperature humidity humidity.abs}
+set readings(dht.wz) {temperature humidity humidity.abs}
+set readings(dht.bad) {temperature humidity humidity.abs}
+set readings(dht.sz) {temperature humidity humidity.abs}
+set readings(dht.studio) {temperature humidity humidity.abs}
+
---
+++ s7.metering.tcl
@@ -0,0 +1,8 @@
+#!/usr/bin/expect
+# set reading(DEVICE) { reading1 reading2 reading3 }
+set readings(s7.metering.floor_heating_return) {state}
+set readings(s7.metering.heatpump_flow) {state}
+set readings(s7.metering.heatpump_return) {state}
+set readings(s7.metering.water_buffer) {state}
+set readings(s7.metering.water_circulation) {state}
+set readings(s7.metering.water_flow) {state}
