d85c1384da96c21676a1841ad3ae0885a8a99cd5
nuccleon <github@claus-beckenbauer.de>
2018-10-19 23:47:44 +0200

    update config

--- dht.tcl
+++ dht.tcl
@@ -1,11 +1,11 @@
 #!/usr/bin/expect
 # set readings(DEVICE) { reading1 reading2 reading3 }
-set readings(dht.aussen) {temperature humidity humidity.abs}
-set readings(dht.lunos) {temperature humidity humidity.abs}
-set readings(dht.k1) {temperature humidity humidity.abs}
-set readings(dht.k3) {temperature humidity humidity.abs}
-set readings(dht.wz) {temperature humidity humidity.abs}
-set readings(dht.bad) {temperature humidity humidity.abs}
-set readings(dht.sz) {temperature humidity humidity.abs}
-set readings(dht.studio) {temperature humidity humidity.abs}
+set readings(dht.aussen) {temperature humidity humidity.abs humidity.diff}
+set readings(dht.lunos) {temperature humidity humidity.abs humidity.diff}
+set readings(dht.k1) {temperature humidity humidity.abs humidity.diff}
+set readings(dht.k3) {temperature humidity humidity.abs humidity.diff}
+set readings(dht.wz) {temperature humidity humidity.abs humidity.diff}
+set readings(dht.bad) {temperature humidity humidity.abs humidity.diff}
+set readings(dht.sz) {temperature humidity humidity.abs humidity.diff}
+set readings(dht.studio) {temperature humidity humidity.abs humidity.diff}
 
--- vaillant.tcl
+++ vaillant.tcl
@@ -1,3 +1,10 @@
 #!/usr/bin/expect
 # set readings(DEVICE) { reading1 reading2 reading3 }
-set readings(vaillant.heizkreise) { bad.valve flur.valve kind1.valve kind2.valve kueche.valve schlafzimmer.valve studio.valve wc.valve wohnzimmer.valve}
+set readings(s7.metering.floor_heating_return) {state}
+set readings(s7.metering.heatpump_flow) {state}
+set readings(s7.metering.heatpump_return) {state}
+set readings(s7.metering.water_buffer) {state}
+set readings(s7.metering.water_circulation) {state}
+set readings(s7.metering.water_flow) {state}
+set readings(vaillant.heizkreise) { summe.valve bad.valve flur.valve kind1.valve kind2.valve kueche.valve schlafzimmer.valve studio.valve wc.valve wohnzimmer.valve}
+set readings(vaillant.470) { Hc1ActualFlowTempDesired }
