0bea3c5711cdcc967170a6ade9925f95b39cab78
nuccleon <github@claus-beckenbauer.de>
2018-10-19 23:42:42 +0200

    removed unneeded

--- s7.metering.tcl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/expect
-# set readings(DEVICE) { reading1 reading2 reading3 }
-set readings(s7.metering.floor_heating_return) {state}
-set readings(s7.metering.heatpump_flow) {state}
-set readings(s7.metering.heatpump_return) {state}
-set readings(s7.metering.water_buffer) {state}
-set readings(s7.metering.water_circulation) {state}
-set readings(s7.metering.water_flow) {state}
