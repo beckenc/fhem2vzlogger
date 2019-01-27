b6b7fb26ecedf9046df3fc91be0a0ccf6de5e400
nuccleon <github@claus-beckenbauer.de>
2019-01-27 23:59:21 +0100

    updated reading names

--- dht.tcl
+++ dht.tcl
@@ -1,11 +1,11 @@
 #!/usr/bin/expect
 # set readings(DEVICE) { reading1 reading2 reading3 }
-set readings(dht.aussen) {temperature humidity humidity.abs humidity.diff}
-set readings(dht.lunos) {temperature humidity humidity.abs humidity.diff}
-set readings(dht.k1) {temperature humidity humidity.abs humidity.diff}
-set readings(dht.k3) {temperature humidity humidity.abs humidity.diff}
-set readings(dht.wz) {temperature humidity humidity.abs humidity.diff}
-set readings(dht.bad) {temperature humidity humidity.abs humidity.diff}
-set readings(dht.sz) {temperature humidity humidity.abs humidity.diff}
-set readings(dht.studio) {temperature humidity humidity.abs humidity.diff}
+set readings(MQTT2_homie_dht_aussen) {temperature humidity humidity_abs humidity_diff}
+set readings(MQTT2_homie_dht_lunos) {temperature humidity humidity_abs humidity_diff}
+set readings(MQTT2_homie_dht_k1) {temperature humidity humidity_abs humidity_diff}
+set readings(MQTT2_homie_dht_k3) {temperature humidity humidity_abs humidity_diff}
+set readings(MQTT2_homie_dht_wz) {temperature humidity humidity_abs humidity_diff}
+set readings(MQTT2_homie_dht_bad) {temperature humidity humidity_abs humidity_diff}
+set readings(MQTT2_homie_dht_sz) {temperature humidity humidity_abs humidity_diff}
+set readings(MQTT2_homie_dht_studio) {temperature humidity humidity_abs humidity_diff}
 
