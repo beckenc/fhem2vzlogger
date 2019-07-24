071c9b61e553f2f569e42929492d3bdc9f332a40
nuccleon <github@claus-beckenbauer.de>
2019-07-24 23:10:26 +0200

    added addition temperature reading

--- dht.tcl
+++ dht.tcl
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 # set readings(DEVICE) { reading1 reading2 reading3 }
-set readings(MQTT2_espeasy_dht_aussen) {temperature humidity}
+set readings(MQTT2_espeasy_dht_aussen) {temperature temperature2 humidity}
 set readings(MQTT2_espeasy_dht_k1) {temperature humidity}
 set readings(MQTT2_espeasy_dht_k3) {temperature humidity}
 set readings(MQTT2_espeasy_dht_wz) {temperature humidity}
@@ -9,4 +9,4 @@ set readings(MQTT2_espeasy_dht_sz) {temperature humidity}
 set readings(MQTT2_espeasy_dht_studio) {temperature humidity}
 set readings(dts_aussen_t1) {temperature}
 set readings(dts_aussen_t2) {temperature}
-set readings(dts_aussen_t1_t2) {temperature temperature_mean temperature_median}
+set readings(dts_aussen_t1_t2) {temperature temperature_smooth}
