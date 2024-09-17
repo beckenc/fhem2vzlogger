3b38f9dba307d03a282d723b0aac3f0a4654e1b3
beckenc <github@claus-beckenbauer.de>
2024-09-17 15:25:26 +0200

    Update tasmota.tcl

--- tasmota.tcl
+++ tasmota.tcl
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 # set readings(DEVICE) { reading1 reading2 reading3 }
-set readings(MQTT2_tasmota_gefrierschrank) {SENSOR_ENERGY_Total}
+set readings(MQTT2_tasmota_kuehlschrank2) {SENSOR_ENERGY_Total}
 set readings(MQTT2_tasmota_kuehlschrank) {SENSOR_ENERGY_Total}
 set readings(MQTT2_tasmota_57) {SENSOR_ENERGY_Total}
 set readings(MQTT2_shelly_wz_audio) {SENSOR_ENERGY_Total}
