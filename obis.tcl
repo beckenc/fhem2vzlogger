6e65a8c285dd0672e1ead7a6d60cf14417552ff0
becken <github@claus-beckenbauer.de>
2025-05-16 09:59:51 +0200

    Enter new power meter

--- obis.tcl
+++ obis.tcl
@@ -1,4 +1,4 @@
 #!/usr/bin/expect
 # set readings(DEVICE) { reading1 reading2 reading3 }
-set readings(OBIS_hausstromzaehler) { T0 T1 }
+set readings(OBIS_hausstromzaehler) { total_consumption total_feed }
 set readings(OBIS_waermestromzaehler) { T0 T1 T2 }
