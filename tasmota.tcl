fe50fd0e415acd75673878f058ce445bdf760624
becken <github@claus-beckenbauer.de>
2025-05-23 09:52:04 +0200

    renamed channels for waermestrom

--- tasmota.tcl
+++ tasmota.tcl
@@ -2,7 +2,6 @@
 # set readings(DEVICE) { reading1 reading2 reading3 }
 set readings(MQTT2_tasmota_kuehlschrank2) {SENSOR_ENERGY_Total}
 set readings(MQTT2_tasmota_kuehlschrank) {SENSOR_ENERGY_Total}
-set readings(MQTT2_tasmota_57) {SENSOR_ENERGY_Total}
 set readings(MQTT2_tasmota_wz_audio) {SENSOR_ENERGY_Total}
 set readings(MQTT2_tasmota_wz_video) {SENSOR_ENERGY_Total}
 set readings(MQTT2_tasmota_balkonkraftwerk) {SENSOR_ENERGY_Total}
